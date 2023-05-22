dee428eaad6c917e5d727fcecf7328a1b2f41b74
Oriol Bernal Martinez <youtfrost@gmail.com>
2023-05-22 21:38:31 +0200

    small fix on deferred

--- WorkingDir/lightShader.glsl
+++ WorkingDir/lightShader.glsl
@@ -30,6 +30,7 @@ in vec2 vTexCoord;
 layout(location=0) out vec4 oColor;
 layout(location=1) out vec4 normal;
 layout(location=2) out vec4 position;
+layout(location=3) out vec4 specularColor;
 layout(location=4) out vec4 brightColor;
 
 uniform vec3 lightColor;
@@ -38,6 +39,7 @@ uniform vec3 intensity;
 void main()
 {
 	oColor = vec4(lightColor * intensity, 1.0);
+	specularColor = vec4(lightColor * intensity, 0.5);
 
 	float brightness = dot(oColor.rgb, vec3(0.2126, 0.7152, 0.0722));
 	if (brightness > 1.0)
